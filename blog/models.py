61b4fcf45fa7611aff4551e0ceff733e81254002
YeonJae Nam <lisanamyj99@gmail.com>
2020-07-27 14:55:40 +0900

    initial django setup 0

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import user
+class Post(models.Model):
+    title=models.charField(max_length=30)
+    content=models.TextField()
 
-# Create your models here.
+    created= models.DateTimeField()
+
+    author=models.ForeignKey()
