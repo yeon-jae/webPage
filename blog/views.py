844c5d42ecf620aba198f40d140d7fabe29cb6d5
yeon-jae <lisanam99@naver.com>
2020-08-07 00:10:31 +0900

    blog post list (index)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from . models import Post
 # Create your views here.
+
+
+def index(request):
+    posts = Post.object.all()
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
