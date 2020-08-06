ed25dea597e2180ae3a6b06e3df35328d0803eb3
yeon-jae <lisanam99@naver.com>
2020-08-07 03:06:28 +0900

    show random image when head_image is none

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,9 @@ class Post (models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created = models.DateTimeField()
+    head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank='True')
 
+    created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
     def __str__(self):
