570ca49f4d57127ca66b06e25003ae8849f523bc
yeon-jae <lisanam99@naver.com>
2020-08-06 23:32:35 +0900

    time:Seol; Post: str

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django. contrib.auth.models import User
 
-class Post(models.Model):
-    title=models.CharField(max_length=30)
-    content= models.TextField()
 
-    created=models.DateTimeField()
+class Post (models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
 
-    author=models.ForeignKey(User, on_delete=True)
+    created = models.DateTimeField()
+
+    author = models.ForeignKey(User, on_delete=True)
+
+
+def __str__(self):
+    return '{} :: {}'.format(self.title, self.author)
