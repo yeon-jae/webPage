932be0a7fbb4d7b0b22e6c350f73b115ce8ca695
yeon-jae <68237481+yeon-jae@users.noreply.github.com>
2022-11-11 22:33:31 +0900

    member_form

---
+++ part3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>프로그래밍입문</title>
+    <link rel="stylesheet" type="text/css" href="./css/common.css">
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+</head>
+<body>
+    <header>
+        <?php include "header.php";?>
+    </header>
+    <section>
+        <?php include "main.php";?>
+    </section>
+    <footer>
+        <?php include "footer.php";?>
+    </footer>
+</body>
+</html>
